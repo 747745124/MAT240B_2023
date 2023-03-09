a1ecc28b02f9e0b03d295ec72f8996c7392891fb
Shaokang Li <zhaonzh@gmail.com>
2023-03-09 10:14:11 -0800

    Update drop.hpp

--- drops/drop.hpp
+++ drops/drop.hpp
@@ -20,6 +20,12 @@ public:
     float Rl;
     float t_init;
     float t_end;
+    float t2_start, t2_end;
+
+    float a_1 = 1.0;
+    float m = 1.33;
+    float q = 1.6;
+    float f = 29;
 
     Drop(float v_term, float H, float x_0, float a)
     {
@@ -31,6 +37,8 @@ public:
         this->Rl = sqrtf((x_0 + a) * (x_0 + a) + H * H);
         this->t_init = this->Rs / c;
         this->t_end = this->Rl / c;
+        this->t2_start = this->t_init + 0.1;
+        this->t2_end = this->t_end + 0.2;
     };
 
     std::vector<float> res;
@@ -41,15 +49,29 @@ public:
             return 0.f;
         }
 
-        if (time_stamp > t_end)
+        if (time_stamp > t_end && time_stamp < t2_start)
         {
             return 0.f;
         }
 
-        float _t = time_stamp;
-        float upper = (c * c * _t * _t - H * H + x_0 * x_0 - a * a);
-        float lower = (2 * x_0 * sqrtf(c * c * _t * _t - H * H));
-        float pressure = rho_0 * c * ke / M_PI * acos(upper / lower);
+        if (time_stamp > t2_end)
+        {
+            return 0.f;
+        }
+
+        float pressure = 0.0f;
+        if (time_stamp < t2_start)
+        {
+            float _t = time_stamp;
+            float upper = (c * c * _t * _t - H * H + x_0 * x_0 - a * a);
+            float lower = (2 * x_0 * sqrtf(c * c * _t * _t - H * H));
+            pressure = rho_0 * c * ke / M_PI * acos(upper / lower);
+        }
+        else
+        {
+            float _t = time_stamp;
+            pressure = 1.7 * a_1 * exp(-m * (_t - q)) * sin(f * (_t - q));
+        }
 
         res.push_back(pressure);
         return pressure;
